c239a0163b08d7dea87bc6ce49d263dc0ac96db2
Mauricio Noreña <baphometh@gmail.com>
2016-11-12 11:28:05 -0500

    changed the routes for competitors to only route index and create

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
-  get 'competitors/index'
-
   root 'pages#game'
+  resources :competitors, only: [:index, :create]
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
